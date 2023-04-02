dcc932046c105e2d61f8099105761a9bcc549fd8
trivedikonda <kondatrivedi@gmail.com>
2023-04-02 14:11:15 +0000

    search suggestions

---
+++ src/components/GoogleSuggestions/index.js
@@ -0,0 +1,64 @@
+// Write your code here
+import {Component} from 'react'
+import SuggestionItem from '../SuggestionItem'
+
+import './index.css'
+
+class GoogleSuggestions extends Component {
+  state = {
+    searchInput: '',
+  }
+
+  onChangeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  showFilteredSuggestions = suggestion => {
+    this.setState({searchInput: suggestion})
+  }
+
+  render() {
+    const {suggestionsList} = this.props
+    const {searchInput} = this.state
+    const filteredList = suggestionsList.filter(each =>
+      each.suggestion.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+    return (
+      <div className="app-container">
+        <div className="container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/google-logo.png"
+            alt="google logo"
+            className="google-logo"
+          />
+        </div>
+        <div className="inner-container">
+          <div className="search-and-input">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
+              alt="search icon"
+              className="search-icon"
+            />
+            <input
+              type="search"
+              onChange={this.onChangeSearchInput}
+              value={searchInput}
+            />
+          </div>
+
+          <ul className="suggestions-list">
+            {filteredList.map(eachSearch => (
+              <SuggestionItem
+                key={eachSearch.id}
+                suggestion={eachSearch.suggestion}
+                showFilteredSuggestions={this.showFilteredSuggestions}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default GoogleSuggestions
---
+++ src/components/SuggestionItem/index.js
@@ -0,0 +1,23 @@
+// Write your code here
+import './index.css'
+
+const SuggestionItem = props => {
+  const {suggestion, showFilteredSuggestions} = props
+
+  const onClickArrow = () => {
+    showFilteredSuggestions(suggestion)
+  }
+  return (
+    <li className="suggestion">
+      <p className="para">{suggestion}</p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+        alt="arrow"
+        className="arrow"
+        onClick={onClickArrow}
+      />
+    </li>
+  )
+}
+
+export default SuggestionItem
